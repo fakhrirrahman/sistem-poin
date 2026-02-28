222eaebf9d9f57f5cde2a30b089b61aa05437a87
fakhrirrahman <fakhrirrahman7@gmail.com>
2026-03-01 00:59:56 +0700

    first commit

---
+++ src/main/java/com/development/sistempoin/SistemPoinApplication.java
@@ -0,0 +1,13 @@
+package com.development.sistempoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemPoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemPoinApplication.class, args);
+    }
+
+}
